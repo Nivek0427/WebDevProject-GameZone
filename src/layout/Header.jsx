cdb62ee93d103d2dfcfd8ffdce4a7d357edc2664
Kevin Vergara Rojas <kavr.60@gmail.com>
2026-09-16 01:42:44 +0000

    creacion header

---
+++ src/layout/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <>
+            <header className="header">
+                <h1>GameZone</h1>
+            </header>
+        </>        
+    );
+}
