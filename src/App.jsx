1ed7c0f92abf95b68d3b9a99703449177bf385dd
Nivek0427 <kavr.60@gmail.com>
2026-09-18 21:39:34 -0500

    agregar header, menu y banner al componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import { Header } from './layout/Header'
+import { Menu } from './layout/Menu'
+import { Banner } from './layout/Banner'
 import './App.css'
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
-      
+      <Menu />
+      <Banner />
 
     </>
   )
 }
 
-export default App
